3bf3dc04e9b450178686e5a1d35a8c2b270766db
Emma <emmalanza@hotmail.com>
2025-05-15 09:00:44 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './styles/styles.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className="text-3xl text-red-700 font-bold underline">Hello Reat + Tailwind</h1>
+    </>
+  )
+}
+
+export default App;
